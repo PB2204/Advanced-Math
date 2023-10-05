8ad1e3eb81773360899bb8d601930af103a4b8a2
saikowsik23 <saikowsik23@gmail.com>
2023-10-05 18:01:41 +0200

    made changes to unit-conversions geometry (even in tests)

--- adv-math/src/units-conversions/units.js
+++ adv-math/src/units-conversions/units.js
@@ -18,9 +18,19 @@ class Units {
     static convertTemperature(value, fromUnit, toUnit) {
         if (fromUnit === 'Celsius' && toUnit === 'Fahrenheit') {
             return (value * 9 / 5) + 32;
+        } else if (fromUnit=='Celsius' && toUnit=='Kelvin') {
+            return value + 273.15;
         } else if (fromUnit === 'Fahrenheit' && toUnit === 'Celsius') {
             return (value - 32) * 5 / 9;
-        } else {
+        } else if (fromUnit=='Fahrenheit' && toUnit=='Kelvin') {
+            return (value + 459.67) * 5 / 9;
+        } else if (fromUnit=='Kelvin' && toUnit=='Celsius') {
+            return value - 273.15;
+        } else if (fromUnit=='Kelvin' && toUnit=='Fahrenheit') {
+            return (value * 9 / 5) - 459.67;
+        }
+
+        else {
             throw new Error('Invalid temperature unit conversion.');
         }
     }
--- adv-math/tests/geometry-trigonometry.test.js
+++ adv-math/tests/geometry-trigonometry.test.js
@@ -17,7 +17,45 @@ describe('Geometry', () => {
     test('calculates the circumference of a circle', () => {
         expect(Geometry.circleCircumference(3)).toBeCloseTo(18.850, 3);
     });
-
+    test('calculates the area of a triangle', () => {
+        expect(Geometry.triangleArea(3, 4)).toBe(6);
+    });
+    test('calculates the volume of a sphere', () => {
+        expect(Geometry.sphereVolume(3)).toBeCloseTo(113.097, 3);
+    });
+    test('calculates the area of an equilateral triangle', () => {
+        expect(Geometry.equilateralTriangleArea(3)).toBeCloseTo(3.897, 3);
+    });
+    test('calculates the area of a triangle given its side lengths', () => {
+        expect(Geometry.triangleArea_sides(3, 4, 5)).toBe(6);
+    });
+    test('calculates the area of a square given its side length', () => {
+        expect(Geometry.squareArea(3)).toBe(9);
+    });
+    test('calculates the perimeter of a square given its side length', () => {
+        expect(Geometry.squarePerimeter(3)).toBe(12);
+    });
+    test('calculates the volume of a cube given its side length', () => {
+        expect(Geometry.cubeVolume(3)).toBe(27);
+    });
+    test('calculates the volume of a rectangular prism given length, width, and height', () => {
+        expect(Geometry.rectangularPrismVolume(3, 4, 5)).toBe(60);
+    });
+    test('calculates the surface area of a rectangular prism given length, width, and height', () => {
+        expect(Geometry.rectangularPrismSurfaceArea(3, 4, 5)).toBe(94);
+    });
+    test('calculates the volume of a cylinder given radius and height', () => {
+        expect(Geometry.cylinderVolume(3, 5)).toBeCloseTo(141.371, 3);
+    });
+    test('calculates the surface area of a cylinder given radius and height', () => {
+        expect(Geometry.cylinderSurfaceArea(3, 5)).toBeCloseTo(150.796, 3);
+    });
+    test('calculates the volume of a cone given radius and height', () => {
+        expect(Geometry.coneVolume(3, 5)).toBeCloseTo(47.123, 3);
+    });
+    test('calculates the surface area of a cone given radius and height', () => {
+        expect(Geometry.coneSurfaceArea(3, 5)).toBeCloseTo(83.229, 3);
+    });
     // Add more test cases as needed
 });
 
