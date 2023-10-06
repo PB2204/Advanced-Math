f9065646791fdffc6de53c3437517a64712cd13c
Matthew Sanner <gravityskunk@gmail.com>
2023-10-05 23:38:31 -0600

    fix: correct length unit conversion

--- adv-math/src/units-conversions/units.js
+++ adv-math/src/units-conversions/units.js
@@ -1,39 +1,33 @@
 class Units {
-    // Method to convert length units (e.g., meters to feet)
-    static convertLength(value, fromUnit, toUnit) {
-        const conversions = {
-            meters: 3.28084, // 1 meter = 3.28084 feet
-            feet: 0.3048,   // 1 foot = 0.3048 meters
-            // Add more unit conversions as needed
-        };
-
-        if (!conversions[fromUnit] || !conversions[toUnit]) {
-            throw new Error('Invalid unit(s).');
-        }
-
-        return value * conversions[fromUnit] / conversions[toUnit];
+  // Method to convert length units (e.g., meters to feet)
+  static convertLength(value, fromUnit, toUnit) {
+    if (fromUnit === "meters" && toUnit === "feet") {
+      return value * 3.28084;
+    } else if (fromUnit === "feet" && toUnit === "meters") {
+      return value * 0.3048;
+    } else {
+      throw new Error("Invalid length unit conversion.");
     }
+  }
 
-    // Method to convert temperature units (e.g., Celsius to Fahrenheit)
-    static convertTemperature(value, fromUnit, toUnit) {
-        if (fromUnit === 'Celsius' && toUnit === 'Fahrenheit') {
-            return (value * 9 / 5) + 32;
-        } else if (fromUnit=='Celsius' && toUnit=='Kelvin') {
-            return value + 273.15;
-        } else if (fromUnit === 'Fahrenheit' && toUnit === 'Celsius') {
-            return (value - 32) * 5 / 9;
-        } else if (fromUnit=='Fahrenheit' && toUnit=='Kelvin') {
-            return (value + 459.67) * 5 / 9;
-        } else if (fromUnit=='Kelvin' && toUnit=='Celsius') {
-            return value - 273.15;
-        } else if (fromUnit=='Kelvin' && toUnit=='Fahrenheit') {
-            return (value * 9 / 5) - 459.67;
-        }
-
-        else {
-            throw new Error('Invalid temperature unit conversion.');
-        }
+  // Method to convert temperature units (e.g., Celsius to Fahrenheit)
+  static convertTemperature(value, fromUnit, toUnit) {
+    if (fromUnit === "Celsius" && toUnit === "Fahrenheit") {
+      return (value * 9) / 5 + 32;
+    } else if (fromUnit == "Celsius" && toUnit == "Kelvin") {
+      return value + 273.15;
+    } else if (fromUnit === "Fahrenheit" && toUnit === "Celsius") {
+      return ((value - 32) * 5) / 9;
+    } else if (fromUnit == "Fahrenheit" && toUnit == "Kelvin") {
+      return ((value + 459.67) * 5) / 9;
+    } else if (fromUnit == "Kelvin" && toUnit == "Celsius") {
+      return value - 273.15;
+    } else if (fromUnit == "Kelvin" && toUnit == "Fahrenheit") {
+      return (value * 9) / 5 - 459.67;
+    } else {
+      throw new Error("Invalid temperature unit conversion.");
     }
+  }
 }
 
 module.exports = Units;
